9a42ec599e8db9564bd230be0aac9eeec3fb64ca
Riddhi Dattatraya Sonawane <80909703+sonawaneriddhi@users.noreply.github.com>
2025-12-06 17:34:01 +0000

    Add files via upload

---
+++ CommandLine_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(int argc, char * argv[])
+{
+    int iCnt = 0;
+
+    printf("Number of command line args are : %d \n",argc);
+
+    for(iCnt = 0; iCnt < argc ; iCnt++)
+    {
+        printf("%s\n",argv[iCnt]);
+    }
+
+    return 0;
+}
